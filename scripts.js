be2ab8db27881da173fd4f3695ef286103ea233e
KingClouderton6 <islandintheclouds@outlook.com>
2022-02-21 17:01:37 +0000

    Setup html/css layout, plotted functions to add books to pg

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+let myLibraryLength = myLibrary.length;
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+};
+
+function addBookToLibrary(){
+    for(let i = 0 ; i < myLibraryLength ; i++){
+        
+    }
+}

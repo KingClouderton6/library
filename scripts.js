d78fbe9922aa533e86e09f6860cd32cfa546711c
KingClouderton6 <islandintheclouds@outlook.com>
2022-02-21 17:45:17 +0000

    Added const to css classes to use DOM, add func to upload cards

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
-let myLibrary = [];
+// let bookCard = document.querySelector('.card');
+let title = document.querySelector('.title');
+let author = document.querySelector('.author');
+let pages = document.querySelector('.book-info');
+let read = document.querySelector('.book-read');
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'Not read');
+
+let myLibrary = [theHobbit];
 let myLibraryLength = myLibrary.length;
 
 function Book(title, author, pages, read){
@@ -8,8 +16,20 @@ function Book(title, author, pages, read){
     this.read = read
 };
 
+const cardContainer = document.querySelector('div.card-container');
+
+function addBookToLibraryTest(){
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card');
+    cardContainer.appendChild(bookCard);
+}
+
+function addContentToCard(){
+
+}
+
 function addBookToLibrary(){
     for(let i = 0 ; i < myLibraryLength ; i++){
-        
+
     }
 }

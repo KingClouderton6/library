effd20d646c306253411e7351aaf696ee4ef20a0
KingClouderton6 <islandintheclouds@outlook.com>
2022-02-22 16:59:26 +0000

    Added function that displays card content

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 // let bookCard = document.querySelector('.card');
-let title = document.querySelector('.title');
-let author = document.querySelector('.author');
-let pages = document.querySelector('.book-info');
-let read = document.querySelector('.book-read');
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'Not read');
+const theWar = new Book('The War', 'Alexander Wiltshire', 363, 'Read');
+const theGreekWar = new Book('The Peloponessian War', 'Thucydides', 584, 'Not read');
 
-let myLibrary = [theHobbit];
+let myLibrary = [theHobbit, theWar, theGreekWar];
 let myLibraryLength = myLibrary.length;
 
 function Book(title, author, pages, read){
@@ -19,14 +17,32 @@ function Book(title, author, pages, read){
 const cardContainer = document.querySelector('div.card-container');
 
 function addBookToLibraryTest(){
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('card');
-    cardContainer.appendChild(bookCard);
+    for (let i = 0 ; i < myLibrary.length ; i++ ){
+        console.log(myLibrary[i].title, myLibrary[i].author);
+        
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('card');
+        cardContainer.appendChild(bookCard);
+        
+        const bookTitle = document.createElement('h2');
+        bookTitle.textContent = myLibrary[i].title
+        bookCard.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('h3');
+        bookAuthor.textContent = myLibrary[i].author
+        bookCard.appendChild(bookAuthor);
+
+        const bookLength = document.createElement('h3');
+        bookLength.textContent = myLibrary[i].pages + ' pages'
+        bookCard.appendChild(bookLength);
+
+        const bookRead = document.createElement('h3');
+        bookRead.textContent = myLibrary[i].read
+        bookCard.appendChild(bookRead);
+
+    }
 }
 
-function addContentToCard(){
-
-}
 
 function addBookToLibrary(){
     for(let i = 0 ; i < myLibraryLength ; i++){
